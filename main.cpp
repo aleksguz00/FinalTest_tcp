8d70785debd5b075484a75513a79fdda56c4fc9d
Aleksandr Guz <113237233+aleksguz00@users.noreply.github.com>
2024-04-26 23:52:37 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main() {
     struct sockaddr_in server_address, client_address;
     char buffer[1024] = {0};
 
-    // Создание сокета
+    // Создаю сокета
     if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         std::cerr << "Socket creation error" << std::endl;
         return -1;
@@ -20,13 +20,13 @@ int main() {
     server_address.sin_addr.s_addr = INADDR_ANY;
     server_address.sin_port = htons(8080);
 
-    // Привязка адреса и порта к сокету
+    // Подключаюсь к сокету
     if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
         std::cerr << "Bind failed" << std::endl;
         return -1;
     }
 
-    // Прослушивание входящих соединений
+    // Запускаю прослушку сокета
     if (listen(server_socket, 3) < 0) {
         std::cerr << "Listen failed" << std::endl;
         return -1;
@@ -35,13 +35,13 @@ int main() {
     int client_socket;
     int client_address_length = sizeof(client_address);
 
-    // Принятие входящего подключения
+    // Сервер принимает подключение (ACK)
     if ((client_socket = accept(server_socket, (struct sockaddr *)&client_address, (socklen_t*)&client_address_length)) < 0) {
         std::cerr << "Accept failed" << std::endl;
         return -1;
     }
 
-    // Обработка сообщений от клиента
+    // Цикл обработки сообщений на сервере от клиента
     while (true) {
         memset(buffer, 0, sizeof(buffer));
         read(client_socket, buffer, 1024);
